4d08699d9168414c66644ae528da3ae1d57ceb9c
rsantos <rodolfo.docarmo@hotmail.com>
2022-12-05 21:44:29 -0300

    Finalização do projeto

--- junior/blogr-landing-page-main/blogr-landing-page-main/js/script.js
+++ junior/blogr-landing-page-main/blogr-landing-page-main/js/script.js
@@ -2,6 +2,7 @@ const btnMenu = document.querySelector('.menu-button');
 const menuNav = document.querySelector('.main-nav');
 
 const subCategory = document.querySelector('.menu-container')
+const activeLists = document.querySelectorAll('.menu-container > li')
 const subMenu = document.querySelector('.submenu')
 
 
@@ -11,5 +12,13 @@ btnMenu.addEventListener('click', (e) => {
 });
 
 subCategory.addEventListener('click', (e) => {
-    e.target.parentNode.classList.toggle('active')
+    for (const list of activeLists) {
+        
+        if (e.target.parentNode.getAttribute('class') ==='active') {
+            break;
+        } else if (list.getAttribute('class') === 'active') {
+            list.classList.toggle('active');
+        }
+    }
+    e.target.parentNode.classList.toggle('active');
 })
