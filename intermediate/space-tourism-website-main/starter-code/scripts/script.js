668aa82c3561f0c91d28901f8d220a59678e9a81
rsantos <rodolfo.docarmo@hotmail.com>
2022-12-26 18:37:52 -0300

    Destinantion page and html of crew page finished

--- intermediate/space-tourism-website-main/starter-code/scripts/script.js
+++ intermediate/space-tourism-website-main/starter-code/scripts/script.js
@@ -9,3 +9,20 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     navBar.classList.toggle('active');
 });
+
+const destList = document.querySelectorAll('.dest-nav li');
+const dest = document.querySelectorAll('body.dest-page section');
+
+destList.forEach((itemList) => {
+    itemList.addEventListener('click', () => {
+        const eleDestiny = itemList.getAttribute('class').split(' ')[0];
+        dest.forEach((item) => {
+            item.classList.remove('active');
+        })
+        dest.forEach((item) => {
+            if (item.getAttribute('class') == eleDestiny) {
+                item.classList.add('active')
+            }
+        })
+    })
+})
