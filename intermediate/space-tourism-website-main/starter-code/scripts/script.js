ec5dea17d1f236f84404f0218288134a2c4076c1
rsantos <rodolfo.docarmo@hotmail.com>
2022-12-31 11:38:21 -0300

    Project finished

--- intermediate/space-tourism-website-main/starter-code/scripts/script.js
+++ intermediate/space-tourism-website-main/starter-code/scripts/script.js
@@ -26,3 +26,37 @@ destList.forEach((itemList) => {
         })
     })
 })
+
+const crewList = document.querySelectorAll('.crew-nav button');
+const crew = document.querySelectorAll('body.crew-page section');
+
+crewList.forEach((itemList) => {
+    itemList.addEventListener('click', () => {
+        const eleCrew = itemList.getAttribute('class').split(' ')[0];
+        crew.forEach((item) => {
+            item.classList.remove('active');
+        })
+        crew.forEach((item) => {
+            if (item.getAttribute('class') == eleCrew) {
+                item.classList.add('active');
+            }
+        })
+    })
+})
+
+const techList = document.querySelectorAll('.tech-nav button');
+const tech = document.querySelectorAll('body.tech-page section');
+
+techList.forEach((itemList) => {
+    itemList.addEventListener('click', () => {
+        const eleCrew = itemList.getAttribute('class').split(' ')[0];
+        tech.forEach((item) => {
+            item.classList.remove('active');
+        })
+        tech.forEach((item) => {
+            if (item.getAttribute('class') == eleCrew) {
+                item.classList.add('active');
+            }
+        })
+    })
+})
